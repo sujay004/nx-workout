0071022714ca03479ad7ff67e09c31a6a0674c6e
sujay kumar <sujaynandhakumar96@gmail.com>
2025-03-07 23:54:57 +0530

    fix: remove RouterModule.forRoot from app.component and use provideRouter instead

--- apps/angular-store/src/app/app.component.ts
+++ apps/angular-store/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { appRoutes } from './app.routes';
 
 @Component({
   standalone: true,
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
